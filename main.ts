f1fa9edf8c2326107be7ade25ea782b8a1c0f906
Littlehanger1234C2 <74791991+Littlehanger1234C2@users.noreply.github.com>
2020-12-04 11:42:07 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,110 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    Car_2.y += -10
+    animation.runImageAnimation(
+    Car_2,
+    [img`
+        . . . . . . e e c c e e . . . . 
+        . . . . . e 2 2 2 2 2 2 e . . . 
+        . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+        . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+        . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+        . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+        . . . e 2 c e c c c c e c 2 e . 
+        . . . e 2 e c b b b b c e 2 e . 
+        . . . e 2 e b b b b b b e 2 e . 
+        . . . e e e e e e e e e e e e . 
+        . . . f e d e e e e e e d e f . 
+        . . . f e 2 d e e e e d 2 e f . 
+        . . . f f e e e e e e e e f f . 
+        . . . . f f . . . . . . f f . . 
+        `],
+    500,
+    false
+    )
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    Car_2.x += -10
+    animation.runImageAnimation(
+    Car_2,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 2 2 2 . . 
+        . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+        . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+        . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+        . d 2 2 4 c b e e e e e e e 2 c 
+        . 2 2 2 4 b e e b b b e b b e 2 
+        . 2 2 2 2 2 e b b b b e b b b e 
+        . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+        . 2 d d 2 e f e e e f e e e e e 
+        . d d 2 e e e f e e f e e e e e 
+        . e e e e e e e f f f e e e e e 
+        . e e e e f f f e e e e f f f f 
+        . . . e f f f f f e e f f f f f 
+        . . . . f f f f . . . . f f f . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    Car_2.x += 10
+    animation.runImageAnimation(
+    Car_2,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 2 2 2 2 2 2 2 2 . . . . 
+        . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+        . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+        . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+        . 2 c 2 e e e e e e e b c 4 2 2 
+        . 2 2 e b b e b b b e e b 4 2 2 
+        . 2 e b b b e b b b b e 2 2 2 2 
+        . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+        . e e e e e e f e e e f e 2 d d 
+        . e e e e e e f e e f e e e 2 d 
+        . e e e e e e f f f e e e e e e 
+        . e f f f f e e e e f f f e e e 
+        . . f f f f f e e f f f f f e . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    Car_2.y += 10
+    animation.runImageAnimation(
+    Car_2,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 2 . . . . 
+        . . . . . 2 2 4 4 2 2 2 2 . . . 
+        . . . . . c 4 2 2 2 2 2 c . . . 
+        . . . . 2 c 4 2 2 2 2 2 c 2 . . 
+        . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+        . . . f 2 c 4 2 2 2 2 2 c 2 f . 
+        . . . f e c 2 2 2 2 2 2 c e f . 
+        . . . f 2 c 2 b b b b 2 c 2 f . 
+        . . . e 2 2 b c c c c b 2 2 e . 
+        . . . e e b c c c c c c b e e . 
+        . . . f e 4 4 4 4 4 4 4 4 e f . 
+        . . . f e d 2 2 2 2 2 2 d e f . 
+        . . . . 2 d d 2 2 2 2 d d 2 f . 
+        . . . . f 2 d 2 2 2 2 d 2 f . . 
+        . . . . . e 2 2 2 2 2 2 e . . . 
+        `],
+    500,
+    false
+    )
 })
 let Car_1: Sprite = null
-let Car_2 = sprites.create(img`
+let Car_2: Sprite = null
+Car_2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . 2 2 2 2 2 2 2 2 . . . . 
     . . . 2 4 2 2 2 2 2 2 c 2 . . . 
@@ -29,15 +122,15 @@ let Car_2 = sprites.create(img`
     . . . f f f . . . . f f f f . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-tiles.setTilemap(tiles.createTilemap(hex`100008000101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010102020202020202020202020202020202`, img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+tiles.setTilemap(tiles.createTilemap(hex`2000080001010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010202020202020202020202020202020202020202020202020202020202020202`, img`
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
     `, [myTiles.transparency16,sprites.vehicle.roadHorizontal,sprites.castle.tileGrass1], TileScale.Sixteen))
 Car_2.setPosition(55, 71)
 game.onUpdateInterval(1000, function () {
